655d6d106885e58c59ac86ebe7de1a6a099d6d13
Mehmood <m.hassan.rahi@gmail.com>
2020-12-30 00:54:42 +0500

    * Added constant files

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,7 @@
 module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
-  singleQuote: true,
-  trailingComma: 'all',
-  tabWidth: 4,
+    bracketSpacing: false,
+    jsxBracketSameLine: true,
+    singleQuote: true,
+    trailingComma: "all",
+    tabWidth: 4,
 };
